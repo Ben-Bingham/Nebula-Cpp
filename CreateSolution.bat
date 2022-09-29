3eb480087c825fdc65cf460e91fce1624244aa97
Ben-Bingham <ben@benbingham.ca>
2022-09-28 21:12:53 -0600

    wip

--- CreateSolution.bat
+++ CreateSolution.bat
@@ -1,8 +1,8 @@
-DEL /S *.sln
-DEL /S *.vcxproj
-DEL /S *.vcxproj.filters
-DEL /S *.vcxproj.user
-DEL /q .vs
+rem DEL /S *.sln
+rem DEL /S *.vcxproj
+rem DEL /S *.vcxproj.filters
+rem DEL /S *.vcxproj.user
+rem DEL /q .vs
 rmdir /q /s build
 
 CALL vendor\premake\premake5.exe vs2022
--- Nebula/src/Main.cpp
+++ Nebula/src/Main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include "Window.h"
+#include "Renderer.h"
+#include "Renderable Objects/Phong/PhongCube.h"
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+	Ruby::Window window{ };
+	Ruby::Renderer renderer{ };
+
+	Ruby::Image red{ Malachite::Vector3f{1.0f, 0.0f, 0.0f} };
+
+	Ruby::Texture diffuseTexture{ red };
+	Ruby::Texture specularTexture{ red };
+
+	Ruby::PhongMaterial cubeMaterial{ diffuseTexture, specularTexture };
+	Ruby::PhongCube cube{ cubeMaterial };
 
 	std::cin.get();
 }
