f72fbf95152ebee0b8a4c51db178e1c491980ad2
Ben-Bingham <ben@benbingham.ca>
2022-12-11 18:31:29 -0700

    changes

--- Nebula/premake5.lua
+++ Nebula/premake5.lua
@@ -1,8 +1,10 @@
+require "../Dependencies/Gemstone/Gemstone/use"
+
 project "Nebula"
 	kind "ConsoleApp"
 	language "C++"
 
-	cppdialect "C++17"
+	cppdialect "C++20"
 
 	targetdir ("%{wks.location}/build/bin/%{prj.name}")
 	objdir ("%{wks.location}/build/bin-int/%{prj.name}")
@@ -14,28 +16,30 @@ project "Nebula"
 
 	includedirs {
 		"src",
-		-- Vendor
-		"%{wks.location}/Dependencies/Gemstone/vendor/GLFW/include",
-		"%{wks.location}/Dependencies/Gemstone/vendor/glew-2.1.0/include",
-		"%{wks.location}/Dependencies/Gemstone/vendor/stb_image",
-		-- Dependencies
-		"%{wks.location}/Dependencies/Gemstone/Malachite/src",
-		"%{wks.location}/Dependencies/Gemstone/Ruby/src",
-		"%{wks.location}/Dependencies/Gemstone/Lazuli/src"
+		-- -- Vendor
+		-- "%{wks.location}/Dependencies/Gemstone/vendor/GLFW/include",
+		-- "%{wks.location}/Dependencies/Gemstone/vendor/glew-2.1.0/include",
+		-- "%{wks.location}/Dependencies/Gemstone/vendor/stb_image",
+		-- -- Dependencies
+		-- "%{wks.location}/Dependencies/Gemstone/Malachite/src",
+		-- "%{wks.location}/Dependencies/Gemstone/Ruby/src",
+		-- "%{wks.location}/Dependencies/Gemstone/Lazuli/src"
 	}
 
-	libdirs {
-		"%{wks.location}/Dependencies/Gemstone/vendor/GLFW/lib-vc2022",
-		"%{wks.location}/Dependencies/Gemstone/vendor/glew-2.1.0/lib/Release/x64"
-	}
+	useGemstone()
 
-	links {
-		-- Vendor
-		"glfw3",
-		"glew32s",
-		"opengl32",
-		-- Dependencies
-		"Malachite",
-		"Ruby",
-		"Lazuli"
-	}
+	-- libdirs {
+	-- 	"%{wks.location}/Dependencies/Gemstone/vendor/GLFW/lib-vc2022",
+	-- 	"%{wks.location}/Dependencies/Gemstone/vendor/glew-2.1.0/lib/Release/x64"
+	-- }
+
+	-- links {
+	-- 	-- Vendor
+	-- 	"glfw3",
+	-- 	"glew32s",
+	-- 	"opengl32",
+	-- 	-- Dependencies
+	-- 	"Malachite",
+	-- 	"Ruby",
+	-- 	"Lazuli"
+	-- }
